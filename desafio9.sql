b91fcd491eb87a2c8c1b6d0b711e5a49d73533a3
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-26 14:23:29 -0300

    Requisito 9 comentario

--- desafio9.sql
+++ desafio9.sql
@@ -3,3 +3,10 @@ FROM SpotifyClone.usuario AS u
 INNER JOIN SpotifyClone.historico_reproducoes AS h
 ON u.usuario_id = h.usuario_id
 WHERE usuario LIKE 'Barbara Liskov'
+
+/*SABENDO O ID DO USUARIO PODERIA USAR SOMENTE A TABELA DO HISTORICO
+
+SELECT COUNT(usuario_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historico_reproducoes 
+WHERE usuario_id = 1
+*/
